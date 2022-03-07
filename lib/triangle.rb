3db88cc8ef4598fa5a3905990c35ff3e489c3669
seth <seth623@gmail.com>
2022-03-07 13:57:02 -0700

    Completed Assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
 class Triangle
-  # write code here
+
+  class TriangleError < StandardError 
+
+  end
+
+  attr_accessor :side_1, :side_2, :side_3 
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2 
+    @side_3 = side_3
+  end 
+
+  def kind 
+
+    validate_triangle 
+     
+    if (@side_1 == @side_2) && (@side_1 == @side_3) && (@side_2 == @side_3)
+      return :equilateral
+    elsif (@side_1 == @side_2) || (@side_1 == @side_3) || (@side_2 == @side_3)
+      return :isosceles
+    elsif (@side_1 != @side_2) && (@side_1 != @side_3) && (@side_2 != @side_3)
+      return :scalene 
+    end
+
+  end 
+
+  def validate_triangle 
+
+    valid = nil 
+
+    not_zero = (@side_1 > 0) && (@side_2 > 0) && (@side_3 > 0) 
+    inequality = (@side_1 + @side_2 > @side_3) && (@side_1 + @side_3 > @side_2) && (@side_2 + @side_3 > @side_1)
+
+    if not_zero && inequality 
+      valid = true 
+    else 
+      valid = false 
+    end 
+      
+    raise TriangleError if valid == false  
+
+  end 
+
 end
